7802ccac423d3aa528e49834bf5d86f6a0afc68e
Caio Braga <caiotabraga@hotmail.com>
2024-05-14 20:59:10 -0300

    Inicio do proheto

---
+++ src/main/java/com/Unimpact/Unimpact/UnimpactApplication.java
@@ -0,0 +1,13 @@
+package com.Unimpact.Unimpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimpactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Unimpact/Unimpact/UnimpactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unimpact.Unimpact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimpactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
