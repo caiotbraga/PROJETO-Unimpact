a58c9285950ff85fff5f97b1a8feb65e6807432e
rayssadavvila <rayssamunizd@gmail.com>
2024-05-19 23:21:57 +0000

    Adicionando estrutura dos checkboxes das escolas da UNICAP e adicionando o script com a regra de seleção

--- src/main/resources/static/js/Fazer_Nova_Solicitação_Contribuinte_Externo.js
+++ src/main/resources/static/js/Fazer_Nova_Solicitação_Contribuinte_Externo.js
@@ -1,7 +1,8 @@
+
 function divPessoaJuridica(){
 	var radioSim = document.getElementById("radio-sim");
 	var radioNão = document.getElementById("radio-nao");
-	var cadastroJuridico = document.getElementById("cadastro-jurico")
+	var cadastroJuridico = document.getElementById("cadastro-juridico")
 
 	if(radioSim.checked == true){
 		cadastroJuridico.style.display = "block"
@@ -11,4 +12,28 @@ function divPessoaJuridica(){
 	}
 }
 
-alert("teste");
+document.addEventListener('DOMContentLoaded', function() {
+    const checkboxes = document.querySelectorAll('.option-checkbox');
+    const maximoPermitido = 2;
+    const elementoMensagem = document.getElementById('messagem_alertando_o_numero_maximo_permitido_de_escolas_selecionadas');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function() {
+            const checkedCheckboxes = document.querySelectorAll('.option-checkbox:checked');
+            if (checkedCheckboxes.length > maximoPermitido) {
+                this.checked = false;
+                mostrarMensagem('Você só pode selecionar até duas opções.');
+            } else {
+                apagarMensagem();
+            }
+        });
+    });
+
+    function mostrarMensagem(mensagem) {
+        elementoMensagem.textContent = mensagem;
+    }
+
+    function apagarMensagem() {
+        elementoMensagem.textContent = '';
+    }
+});
