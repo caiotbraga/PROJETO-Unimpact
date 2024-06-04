2db742c528c50625296a371b6cfec30b447d7218
Victor Araújo <gabrielpearaujo20@gmail.com>
2024-06-04 14:00:46 -0300

    Adicionando JS

---
+++ src/main/resources/static/solicitante/js/FazerNovaSolicitacaoContribuinteExterno.js
@@ -0,0 +1,39 @@
+
+function divPessoaJuridica(){
+	var radioSim = document.getElementById("radio-sim");
+	var radioNao = document.getElementById("radio-nao");
+	var cadastroJuridico = document.getElementById("cadastro-juridico");
+
+	if(radioSim.checked == true){
+		cadastroJuridico.style.display = "block"
+	}
+	else{
+		cadastroJuridico.style.display = "none"
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const checkboxes = document.querySelectorAll('.option-checkbox');
+    const maximoPermitido = 2;
+    const elementoMensagem = document.getElementById('messagem_alertando_o_numero_maximo_permitido_de_escolas_selecionadas');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function() {
+            const checkedCheckboxes = document.querySelectorAll('.option-checkbox:checked');
+            if (checkedCheckboxes.length > maximoPermitido) {
+				this.checked = false
+                mostrarMensagem('Você só pode selecionar até duas opções.');
+            } else {
+                apagarMensagem();
+            }
+        });
+    });
+
+    function mostrarMensagem(mensagem) {
+        elementoMensagem.textContent = mensagem;
+    }
+
+    function apagarMensagem() {
+        elementoMensagem.textContent = '';
+    }
+});
